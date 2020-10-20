3d8b8c7edf031773bb9b47895cf9ba3e61c38cab
Farhan Ahmed <fahmed96@uwo.ca>
2020-10-20 00:28:47 -0400

    Making example simpler and readable

--- EventSchedulerExample/BankAccount.py
+++ EventSchedulerExample/BankAccount.py
@@ -6,20 +6,20 @@ class BankAccount:
     def deposit(self, deposit, result_handler):
         if deposit > 0:
             self.balance += deposit
-            result_handler(0)  # return code of 0 indicates successful
+            result_handler(True)
             print("You have deposited: " + str(deposit))
             print("The new balance is: " + str(self.balance) + "\n")
             return
-        result_handler(-1)  # return code of -1 indicates failure
+        result_handler(False)
         print("Must deposit a positive amount \n")
 
     def withdraw(self, withdrawal, result_handler):
         if withdrawal <= self.balance:
             self.balance -= withdrawal
-            result_handler(0)  # return code of 0 indicates successful
+            result_handler(True)
             print("You have withdrawn: " + str(withdrawal))
             print("The new balance is: " + str(self.balance) + "\n")
             return
-        result_handler(-1)  # return code of -1 indicates failure
+        result_handler(False)
         print("Insufficient funds \n")
 
--- EventSchedulerExample/Transactions.py
+++ EventSchedulerExample/Transactions.py
@@ -1,39 +1,35 @@
 import BankAccount
 from EventScheduler_pkg.EventScheduler import EventScheduler
-import threading
 
-event_scheduler = EventScheduler("transaction_threads")
-event_scheduler.start()
+scheduler = EventScheduler("transaction_threads")
+scheduler.start()
 account = BankAccount.BankAccount(100)
 
 
 def atm_chicago():
-    def is_transaction_successful(return_code):
-        if return_code == 0:
+    def is_transaction_successful(successful):
+        if successful:
             print("Chicago ATM Transaction Successful")
         else:
             print("Chicago ATM Transaction Failed")
 
-    event_scheduler.enter(1, 1, account.withdraw, [90, is_transaction_successful]) # note priority is at 1
+    scheduler.enter(1, 1, account.withdraw, [90, is_transaction_successful])  # note priority is at 1
 
 
 def atm_los_angeles():
-    def is_transaction_successful(return_code):
-        if return_code == 0:
+    def is_transaction_successful(successful):
+        if successful:
             print("Los Angeles ATM Transaction Successful")
         else:
             print("Los Angeles ATM Transaction Failed")
 
-    event_scheduler.enter(1, 0, account.withdraw, [20, is_transaction_successful]) #note priority here is 0
+    scheduler.enter(1, 0, account.withdraw, [20, is_transaction_successful])  # note priority here is 0
 
 
 # Example 1 since Los Angeles has higher priority it will execute first
-thread_atm_chicago = threading.Thread(target=atm_chicago, name="ATM Chicago")
-thread_atm_los_angeles = threading.Thread(target=atm_los_angeles, name="ATM Los Angeles")
-thread_atm_chicago.start()
-thread_atm_los_angeles.start()
-thread_atm_chicago.join()
-thread_atm_los_angeles.join()
+atm_chicago()
+atm_los_angeles()
+
 '''
 Los Angeles ATM Transaction Successful
 You have withdrawn: 20
@@ -42,4 +38,4 @@ The new balance is: 80
 Chicago ATM Transaction Failed
 Insufficient funds
 '''
-event_scheduler.stop()
+scheduler.stop()
