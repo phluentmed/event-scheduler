d45fbdaf3e2459ff368e61667a71dc5f2a8c8163
madalitsomchaina <mmchain@uwo.ca>
2020-12-29 19:36:17 -0500

    fix test invocation

--- event_scheduler_tests/tests.py
+++ event_scheduler_tests/tests.py
@@ -1,5 +1,7 @@
 import unittest
 
-unit_tests = unittest.TestLoader().discover('unit_tests', '*tests.py')
+unit_tests = unittest.TestLoader().discover('event_scheduler_tests/unit_tests',
+                                            '*tests.py',
+                                            '.')
 
 unittest.TextTestRunner().run(unit_tests)
